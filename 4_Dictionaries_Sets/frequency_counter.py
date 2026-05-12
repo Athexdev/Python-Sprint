e4f63da4755e26a0df5c74ddd82e4de5cf44c12a
DEBESH NAYAK <debeshnayak466@gmail.com>
2026-05-12 11:45:14 +0530

    Update frequency_counter.py

--- 4_Dictionaries_Sets/frequency_counter.py
+++ 4_Dictionaries_Sets/frequency_counter.py
@@ -33,3 +33,38 @@ for i in purchases:
         count[i]=1
 print(list(set(purchases)))       
 print(count)
+
+
+**Bonus Program**
+  try:
+    print("NOTE : This program can handle every input !!")
+    s=input("enter your word:")
+    s=s.lower()
+    d={}
+    for i in s:
+        if i in d:
+            d[i]=d[i]+1
+        else:
+            d[i]=1
+    i=0
+    j=len(s)-1
+    status=True
+
+    while i<j:
+        if s[i]!=s[j]:
+            status=False
+            break
+        else:
+            i=i+1
+            j=j-1
+    if status==True:
+        print("Status : Palindrome",
+        "\n--> Lenth of your word is:",len(s),
+        "\n--> More Details:",d)
+    else:
+        print("Status :Not Palindrome",
+        "\n--> Lenth of your word is:",len(s),
+        "\n--> More Details:",d)
+    
+except Exception:
+    print("kindly check your program and try again !!")
